67952c98e63f84c4596ea9bd02b69b19d4766790
Amrito | アンブロシア <bhabanidas431@gmail.com>
2025-05-07 18:35:26 +0530

    garnet: Move some init files into device tree
    * init.kernel.post_boot.sh
    * init.kernel.post_boot-parrot.sh
    
    Signed-off-by: Amrito | アンブロシア <bhabanidas431@gmail.com>

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -2098,8 +2098,6 @@ PRODUCT_PACKAGES += \
     imsdaemon \
     init.class_main \
     init.crda \
-    init.kernel.post_boot-parrot \
-    init.kernel.post_boot \
     init.mdm \
     init.qcom.class_core \
     init.qcom.coex \
